5e1860549b04934fe2a215dd131e38906e6a281e
Vulcanric <jneric49@gmail.com>
2023-02-19 19:31:51 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,32 +3,26 @@
 /**
  * main - Entry point
  *
- * Description: Prints all possible combinations of single-digit numbers
- * separated by ", "
- *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i, j;
+int i, j;
 
 	for (i = 0; i < 10; i++)
-	{
-		for (j = i + 1; j < 10; j++)
-		{
-			putchar(i + '0');
-			putchar(',');
-			putchar(' ');
-			putchar(j + '0');
-
-			if (i != 8 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
+{
+	for (j = i + 1; j < 10; j++)
+{
+	putchar(i + '0');
+	putchar(j + '0');
+	if (i != 8 || j != 9)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+}
 
 	putchar('\n');
 
