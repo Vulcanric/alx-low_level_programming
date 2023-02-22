158b562c39d632515cf62f4be6e96e1da008822f
Vulcanric <jneric49@gmail.com>
2023-02-21 20:26:21 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ * */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+{
+	_putchar(letter);
+	letter++;
+}
+
+	_putchar('\n');
+}
+
