9e0034c4a71cce51223c70be0a9162622ce88c17
Vulcanric <jneric49@gmail.com>
2023-02-21 18:45:43 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,10 @@
-#include "main.h"
+/* _putchar.c */
 
-int main(void)
+#include "main.h"
+#include <unistd.h>
+
+void _putchar(char c)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    
-    return (0);
+    write(1, &c, 1);
 }
 
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+/* main.h */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int main(void);
+void _putchar(char c);
+
+#endif /* MAIN_H */
+
