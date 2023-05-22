2775ad02a3d98ce49583704ad9ca7e056b10b23b
Vulcanric <jneric49@gmail.com>
2023-05-22 04:36:20 -0700

    Prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,24 +10,27 @@ int main(void)
 	int n1, n2, n3; /* Declare variables first-num, second-num and third-num */
 	n1 = 0; /* Initialize first-num to be zero */
 
-	/* while first-num is 0 and is less than 10,
-	 * assign second-num to be equal to first-num added by 1
-	 * satisfy the second loop before incrementing first-num*/
+	/** while first-num is 0 and is less than 10,
+	  * assign second-num to be equal to first-num added by 1
+	  * satisfy the second loop before incrementing first-num
+	  */
 	while (n1 < 10)
 	{
 		n2 = n1 + 1;
 		
-		/* while second-num is plus 1 greater than first-num and is less than 10
-		 * assign third-num to be equal to second-num added by 1
-		 * satisfy the third loop before incrementing second-num */
+		/** while second-num is plus 1 greater than first-num and is less than 10
+		  * assign third-num to be equal to second-num added by 1
+		  * satisfy the third loop before incrementing second-num
+		  */
 		while (n2 < 10)
 		{
 			n3 = n2 + 1;
 
-			/* while third-num is plus 1 greater than second-num and is less than 10
-			 * put the character first-num + 48 to stdout
-			 * put the character second-num + 48 to stdout
-			 * put the character third-num + 48 to stdout */
+			/** while third-num is plus 1 greater than second-num and is less than 10
+			  * put the character first-num + 48 to stdout
+			  * put the character second-num + 48 to stdout
+			  * put the character third-num + 48 to stdout
+			  */
 			while (n3 < 10)
 			{
 				putchar(n1 + 48);
