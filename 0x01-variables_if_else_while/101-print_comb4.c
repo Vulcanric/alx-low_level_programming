8f0cbb6e9d8ef259d5bd83e0d862d3e3e30956a8
Vulcanric <jneric49@gmail.com>
2023-05-22 13:49:35 -0700

    A program that prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,20 +9,20 @@ int main(void)
 {
 	int n1, n2, n3; /* Declare variables first-num, second-num and third-num */
 
-	n1 = 0; /* Initialize first-num to be zero */
-	while (n1 < 10)
+	n1 = 48; /* Initialize first-num to be 48; is decimal rep. for '0' */
+	while (n1 < 58) /* 58 is the decimal rep. for '10' */
 	{
 		n2 = n1 + 1;
 
-		while (n2 < 10)
+		while (n2 < 58)
 		{
 			n3 = n2 + 1;
 
-			while (n3 < 10)
+			while (n3 < 58)
 			{
-				putchar(n1 + 48); /* 48 is ascii decimal value for the character '0' */
-				putchar(n2 + 48);
-				putchar(n3 + 48);
+				putchar(n1);
+				putchar(n2);
+				putchar(n3);
 				if (n1 == 7 && n2 == 8 && n3 == 9)
 					n1++; /* THEN increment first-num */
 				else
