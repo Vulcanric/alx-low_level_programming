a8ff8b9610fb6f093a266e9f1b6be7e7d378df40
Vulcanric <jneric49@gmail.com>
2023-06-02 03:27:10 -0700

    Function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,19 +7,18 @@
  */
 char *cap_string(char *str)
 {
-	int i; /* Character index */
-	int plc_num; /* Place-number of letter */
+	int i = 0; /* Character index */
+	int plc_num = str[i] - 'a'; /* Place-number of letter */
 
-	i = 0;
-	while (str[i] != '\0')
+	/* IF the 1st character of string is lowercase */
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] = plc_num + 'A'; /* Change to upper */
+
+	for (; str[i] != '\0'; i++)
 	{
-		plc_num = str[i] - 'a'; /* letter at index minus letter 'a' */
-		if (str[0] >= 'a' && str[0] <= 'z')
-			str[0] = plc_num + 'A';
-
 		plc_num = str[i + 1] - 'a'; /* Next letter minus 'a' */
 		/* Checking seperators of words */
-		else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '.')
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '.')
 		{
 			/* IF the next letter is a lowercase letter */
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
@@ -32,13 +31,12 @@ char *cap_string(char *str)
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] = plc_num + 'A'; /* Change to upper */
 		}
-		else if (str[i] == '?' || str[i] == '(' || str[i] == ')' || str[i] == '{')
+		else if (str[i] == '}' || str[i] == '(' || str[i] == ')' || str[i] == '{')
 		{
 			/* IF the next letter is a lowercase letter */
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] = plc_num + 'A'; /* Change to upper */
 		}
-		i++;
 	}
 
 	return (str);
