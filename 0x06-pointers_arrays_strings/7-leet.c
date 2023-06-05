962bda4f75e9219626f4a9d887c3f5b1d7298e72
Vulcanric <jneric49@gmail.com>
2023-06-04 17:36:29 -0700

    Function that encodes a string into 1337(leet)

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,24 +7,17 @@
  */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char leetchar[] = "aAeEoOlLtT";
+	char leetnum[] = "4433001177";
 
 	/* FOR LOOP to run through each character of the string */
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		/* while the character is an alphabet */
-		for (; (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'); i++)
+		for (j = 0; j < 10; j++)
 		{
-			if (s[i] == 'a' || s[i] == 'A') /* IF character is a/A*/
-				s[i] = 4 + '0'; /* Change the value to 4 */
-			else if (s[i] == 'e' || s[i] == 'E') /* ELSE IF character is e/E*/
-				s[i] = 3 + '0'; /* Change the value to 3 */
-			else if (s[i] == 'o' || s[i] == 'O') /* ELSE IF character is o/O*/
-				s[i] = 0 + '0'; /* Change the value to 0 */
-			else if (s[i] == 't' || s[i] == 'T') /* ELSE IF character is t/T*/
-				s[i] = 7 + '0'; /* Change the value to 7 */
-			else if (s[i] == 'l' || s[i] == 'L') /* ELSE IF character is l/L*/
-				s[i] = 1 + '0'; /* Change the value to 1 */
+			if (s[i] == leetchar[j])
+				s[i] = leetnum[j];
 		}
 	}
 	return (s); /* return encoded string */
