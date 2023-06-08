6d9ba25e5b8d1907110713fa29486722a242db0f
Vulcanric <jneric49@gmail.com>
2023-06-08 08:15:11 -0700

    Function that returns the value of 'x' raised to the power 'y'

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,10 +11,10 @@ int _pow_recursion(int x, int y)
 	if (y < 0) /* IF y is lower than 0 */
 		return (-1); /* return -1 as exponential */
 	if (y == 0) /* ELSE IF y is zero */
-		return (1); /* Set base value for function */
-	else
 	{
+		return (1); /* Set base value for function */
+	}
+	else
 	/* Multiply x until x * base value */
 		return (x * _pow_recursion(x, y - 1));
-	}
 }
