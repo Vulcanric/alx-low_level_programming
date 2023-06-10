c4f43397f5fb0f85ceff0b03130f7c4ea144728e
Vulcanric <jneric49@gmail.com>
2023-06-09 19:43:26 -0700

    Converts a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,7 +15,7 @@ int _atoi(char *s)
 	i = 0;
 	/* Handling signs */
 		/* WHILE character is not null and is not a number */
-	while (s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'))
+	while (s[i] && !(s[i] >= '0' && s[i] <= '9'))
 	{
 		if (s[i] == '-') /* IF character is minus */
 			sign *= -1; /* Let sign be whatever sign was before * -1 */
@@ -24,12 +24,12 @@ int _atoi(char *s)
 
 	/* Handling numbers */
 		/* WHILE character is not null and is a number('0' - '9') */
-	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+	while (s[i] && (s[i] >= '0' && s[i] <= '9'))
 	{
 		res = res * 10 + (s[i] - '0');
 		i++;
 	}
-	
+
 	/* Applying sign to my result by multiply the sign gotten by result */
 	res *= sign;
 
