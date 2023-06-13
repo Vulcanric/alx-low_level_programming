437df6e87b2fda13c9db960cc120d8ab882b0284
Vulcanric <jneric49@gmail.com>
2023-06-13 01:51:27 -0700

    Program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,18 +11,30 @@
 int main(int argc, char *argv[])
 {
 	int sum = 0, i;
+	int status, j;
 
 	if (argc > 1) /* IF the number of argument passed is more than one */
 	{
 		for (i = 1; i < argc; i++) /* FOR loop to go through all arguments */
 		{
 			/* IF the argument is a non-digit symbol ... */
-			if (!(*argv[i] >= '0' && *argv[i] <= '9'))
+			for (j = 0; argv[i][j]; j++)
 			{
-				printf("Error\n"); /* Print error and return 1 */
+				if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+				{
+					status = 0;
+					break;
+				}
+				else if (argv[i][j] >= '0' && argv[i][j] <= '9')
+					status = 1;
+			}
+
+			if (status == 0) /* IF argument is a non-digit symbol*/
+			{
+				printf("Error\n"); /* Print error and return 1*/
 				return (1);
 			}
-			else /* ELSE if it is a digit */
+			else /* ELSE if it is a digit ... */
 				sum += atoi(argv[i]); /* Sum them */
 		}
 		printf("%d\n", sum); /* Print sum*/
