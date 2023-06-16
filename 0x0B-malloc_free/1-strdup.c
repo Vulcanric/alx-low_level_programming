897288e70e08bc0e5652624368ebfc8a440d5097
Vulcanric <jneric49@gmail.com>
2023-06-15 17:06:22 -0700

    Function that creates a duplicate copy of a string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,27 +3,28 @@
 #include <string.h>
 
 /**
- * _strdup - Creates a duplicate copy of the string in a seperate block of memory
+ * _strdup - Creates a duplicate copy of the string in a seperate
+ * block of memory
  * @str: Pointer to the string to duplicate
  * Return: Pointer to the new string, or NULL if 'str' is NULL
  */
 char *_strdup(char *str)
 {
-	unsigned int i, size = strlen(str) + 1;
+	unsigned int i = 0;
 	char *new_str;
 
 	if (str == NULL)
 		return (NULL);
-
-	new_str = malloc(sizeof(*str) * size);
+	while (str[i] != '\0')
+		i++;
+	new_str = malloc(sizeof(*str) * (i + 1));
 
 	if (new_str == NULL)
 		return (NULL);
 
-	for (i = 0; i < size && *str; i++)
+	for (i = 0; str[i]; i++)
 	{
-		new_str[i] = *str;
-		str++;
+		new_str[i] = str[i];
 	}
 	return (new_str);
 }
