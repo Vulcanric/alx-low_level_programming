1a923e970580da8887e8427b4d24e80d9ee43ad6
Vulcanric <jneric49@gmail.com>
2023-06-20 23:30:10 -0700

    Function that initializes a structure typed variable

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,6 +14,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 /*	if (d == NULL)
 		d = malloc(sizeof(struct dog));*/
+
 	/* Initializing members of the variable */
 	d->name = name;
 	d->age = age;
