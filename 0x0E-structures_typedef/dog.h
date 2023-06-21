16abec1af059a5428c7dbc3cb3cffef82a9da024
Vulcanric <jneric49@gmail.com>
2023-06-20 21:34:14 -0700

    Defined a new type named struct dog, having elements name, age and owner

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - User-defined type
+ * @name: element1
+ * @age: element2
+ * @owner: element3
+ *
+ * Description: New user-defined type having three members.
+ * All relating to a dog
+ */
 struct dog
 {
 	char *name;
