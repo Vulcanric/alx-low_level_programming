4ddec7ed8ee69c2d8793149308c08b182b314665
Vulcanric <jneric49@gmail.com>
2023-06-21 04:03:19 -0700

    User-defined type 'dog' having elements name, age and owner

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -29,4 +29,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+/* Other helpful prototypes */
+char *_strcpy(char *des, char *src);
+unsigned int _strlen(char *s);
 #endif
