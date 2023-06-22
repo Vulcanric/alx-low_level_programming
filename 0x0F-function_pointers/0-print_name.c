c7b5c3836079c3dabf8835f012a2f9833458d767
Vulcanric <jneric49@gmail.com>
2023-06-21 21:54:50 -0700

    Prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name
+ * @name: Pointer to name(string) to print
+ * @f: Pointer to function to call back
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name); /* Calling back the function that f is pointing to */
+}
