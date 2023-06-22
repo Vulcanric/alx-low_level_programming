22990ff0b83026c6480fade06589be03141f9778
Vulcanric <jneric49@gmail.com>
2023-06-22 05:54:26 -0700

    Function that searches for an integer in an array's element list

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -23,12 +23,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) == FALSE) /* IF cmp results to 0 */
-		{
-			continue; /* move to the next integer in list */
-			return (-1);
-		}
+			i++; /* move to the next integer in list */
 		else
-			break; /* break to avoid more than one inclusion and */
+			break; /* break to avoid more than one inclusion */
+	}
+
+	/* IF no matching character was found until i reaches the end */
+	if (i == size)
+	{
+		return (-1);
 	}
 
 	return (i); /* return the index where the integer was found */
