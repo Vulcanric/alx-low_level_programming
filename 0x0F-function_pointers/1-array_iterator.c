20b7c416d1f04fe1e96b9be116f5e37e48c0afb6
Vulcanric <jneric49@gmail.com>
2023-06-21 22:15:09 -0700

    Iterates through all elements of an array of integers printing each using a function pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Iterate through all elements of an array and prints them
+ * @array: Pointer to the array to iterate
+ * @size: Size of the array
+ * @action: Pointer to the function to use
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i = 0; /* Same as size_t */
+
+	while (i < size) /* Iterating through all elements */
+	{
+		action(array[i]); /* Perform function pointed to by action */
+		i++;
+	}
+}
