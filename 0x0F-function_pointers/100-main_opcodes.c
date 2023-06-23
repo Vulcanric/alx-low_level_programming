45f86c25193f0c6bb38d96d5ac81162d6fd5f20e
Vulcanric <jneric49@gmail.com>
2023-06-23 07:39:02 -0700

    Prints the opcodes of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints this program opcodes
+ * @argc: count of argument
+ * @argv: Pointer to the argument
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *opcode_byte;
+	int numbyte;
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	numbyte = atoi(argv[1]);
+	if (numbyte < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	opcode_byte = (char *)main;
+	for (i = 0; i < numbyte; i++)
+	{
+		printf("%02hhx", opcode_byte[i]);
+		if (i == (numbyte - 1))
+			printf("\n");
+		printf(" ");
+	}
+
+	return (0);
+}
