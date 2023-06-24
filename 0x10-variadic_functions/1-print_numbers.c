6ba29482db15baadf801dd3e249971470020379c
Vulcanric <jneric49@gmail.com>
2023-06-23 18:15:24 -0700

    Created a function that prints numbers passed to it using variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,18 +14,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list numlist;
 
-	if (n == 0)
-		return;
-
-	va_start(numlist, n);
-
-	for (i = 0; i < n && separator != NULL; i++)
+	while (separator != NULL || n != 0)
 	{
-		printf("%d", va_arg(numlist, int));
-		if (i != (n - 1))
-			printf("%s", separator);
-		else
-			printf("\n");
+		va_start(numlist, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(numlist, int));
+			if (i != (n - 1))
+				printf("%s", separator);
+			else
+				printf("\n");
+		}
 	}
 
 	va_end(numlist);
