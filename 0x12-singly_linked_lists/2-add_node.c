2e46350a43795858f51def41c5b5a13a6783c488
Vulcanric <jneric49@gmail.com>
2023-07-05 14:43:28 -0700

    Function that adds a node at the beginning created

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,12 +10,12 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str); 
+	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 		new_node->str = "(nil)";
 	new_node->len = _strlen(str);
@@ -37,7 +37,7 @@ size_t _strlen(const char *str)
 	while (*str != '\0')
 	{
 		counter++;
-		str++;
+		sr++;
 	}
 	return (counter);
 }
