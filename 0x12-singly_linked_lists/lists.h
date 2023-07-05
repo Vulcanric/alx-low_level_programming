92ac4542e033c0a87af43e123517518a6c4ed590
Vulcanric <jneric49@gmail.com>
2023-07-05 12:45:23 -0700

    Header file updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -17,4 +18,6 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif
