ec2206807b3e555067f68e210a16dd3e2a390f79
Vulcanric <jneric49@gmail.com>
2023-07-06 03:27:18 -0700

    Function that adds node to the end of a list_t list created

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -5,42 +5,36 @@
  * @head: pointer to the head node of the list
  * @str: the string to be assigned to the str member
  * of a list_t node
- * Return: pointer to the new node created
+ * Return: pointer to the new node created, or null if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new_node, *ptr = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
 
 	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL || *head == NULL)
+
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 		new_node->str = "(nil)";
-
-	new_node->len = _strlen(str);
+	new_node->len = len;
 	new_node->next = NULL;
-	(*head)->next = new_node;
-	*head = new_node;
 
-	return (new_node);
-}
+	if (*head == NULL)
+		*head = new_node;
 
-/**
- * _strlen - Counts the number of characters contained in a string
- * @str: the string to count
- * Return: the count of chars in the string
- */
-size_t _strlen(const char *str)
-{
-	unsigned int counter = 0;
-
-	while (*str != '\0')
+	else
 	{
-		counter++;
-		str++;
+		while (ptr->next != NULL)
+			ptr = ptr->next;
+		ptr->next = new_node;
 	}
 
-	return (counter);
+	return (new_node);
 }
