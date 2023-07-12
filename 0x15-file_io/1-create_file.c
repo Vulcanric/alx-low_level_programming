240fb81f483af13d7527004feaafd80ec2e0507c
Vulcanric <jneric49@gmail.com>
2023-07-12 16:55:53 -0700

    Implement a function that creates a file with read and write mode

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,9 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	n = write(fd, text_content, textlen);
+	if (text_content)
+		n = write(fd, text_content, textlen);
+
 	if (n == -1)
 		return (-1);
 
