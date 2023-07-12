3071b1a43a4c64c701167aa18d718bd684a528a1
Vulcanric <jneric49@gmail.com>
2023-07-12 16:18:53 -0700

    Header file updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Used header file */
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* Prototypes of the functions */
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
