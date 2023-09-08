c4546e408a356902fe3adb0cd7be7058adaab120
Vulcanric <jneric49@gmail.com>
2023-09-08 22:31:41 +0100

    creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,11 +7,14 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+	hash_table_t *table = NULL;
 
-	table = malloc(sizeof(hash_table_t) * size);
-	if (!table)
-		return (NULL);
+	if (size != 0)
+	{
+		table = malloc(sizeof(hash_table_t) * size);
+		if (!table)
+			return (NULL);
+	}
 
 	return (table);
 }
