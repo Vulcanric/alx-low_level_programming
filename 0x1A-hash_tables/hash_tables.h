56105fc2fca01e81d2961c3edd6edb0ee108cf7a
Vulcanric <jneric49@gmail.com>
2023-09-08 22:27:47 +0100

    updated: added important header files

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,8 @@
 #ifndef HASH_TABLE_H
 #define HASH_TABLE_H
 
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -30,6 +32,7 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
+
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
