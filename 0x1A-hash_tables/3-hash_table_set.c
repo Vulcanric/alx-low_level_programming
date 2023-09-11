c6d354ff5b5ecd0a9cd709062a5e5d68957fa002
Vulcanric <jneric49@gmail.com>
2023-09-11 18:03:10 +0100

    inserts an item into the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,23 @@
 #include "hash_tables.h"
 
 /**
+ * free_item - frees items node
+ * @node: item to free
+ * Return: nothing
+ */
+void free_item(hash_node_t *node)
+{
+	free(node->key);
+
+	if (node->value)
+		free(node->value);
+
+	if (node->next)
+		free(node->next);
+	free(node);
+}
+
+/**
  * hash_table_set - adds an element to the hash table
  * @ht: pointer to a hash table
  * @key: string key
@@ -9,21 +26,44 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
-	char *value_cp = NULL;
+	hash_node_t *new_item, *curr_item;
+	unsigned long int index;
 
-	new = malloc(sizeof(hash_node_t));
-	if (!new)
+	new_item = malloc(sizeof(hash_node_t));
+	if (!new_item)
 		return (0);
 
+	new_item->key = (char *)malloc(sizeof(char) * strlen(key));
+	strcpy(new_item->key, key);
 	if (value)
 	{
-		value_cp = malloc(sizeof(char) * strlen(value));
-		if (!value_cp)
-			return (0);
-		strcpy(value_cp, value);
+		new_item->value = (char *)malloc(sizeof(char) * strlen(value));
+		strcpy(new_item->value, value);
 	}
-	new->value = value_cp;
+	new_item->next = NULL;
 
-	if (!ht->array)
+	/* getting the index, where to store key-value */
+	index = key_index((const unsigned char *)new_item->key, ht->size);
+	curr_item = ht->array[index];
+
+	if (curr_item == NULL) /* empty bucket */
+		ht->array[index] = new_item;
+	else
+	{
+		/* Handling collisions and updating of values */
+		while (curr_item->next && strcmp(curr_item->key, key) != 0)
+			curr_item = curr_item->next;
+
+		/* If the loop breaks before current item is null */
+		/* meaning the key matched. Update key value */
+		if (curr_item->next != NULL)
+		{/* clear former memory and update with new value */
+			free(curr_item->value);
+			curr_item->value = new_item->value;
+			free_item(new_item);  /* no longer needed */
+		}
+		else
+			curr_item->next = new_item;
+	}
+	return (0);
 }
