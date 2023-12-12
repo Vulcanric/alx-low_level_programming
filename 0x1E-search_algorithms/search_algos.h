8fb736bfcbe9c7b0c05c470481f593c77757ee36
Vulcanric <jneric49@gmail.com>
2023-12-12 06:56:59 +0100

    Created header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,56 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+
+#define NOT_FOUND -1
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/* Prototypes of listint_t */
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
+
+/**
+ * struct skiplist_s - Singly linked list with an expression lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: Singly linked list structure with an express lane
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+/* Prototypes of skiplist_t */
+skiplist_t *linear_skip(skiplist_t *list, int value);
